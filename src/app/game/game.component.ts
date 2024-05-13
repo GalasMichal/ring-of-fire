dab8fdf07b8c92ef29b68bd0c8e9c3c3ca8eb975
Michal Galas <michalgalas1989@gmail.com>
2024-05-13 15:20:04 +0200

    created start screen, added cards stack and some animations

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
+
+@Component({
+  selector: 'app-game',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.scss'
+})
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  
+
+  constructor(){}
+  game: Game = new Game;
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame(){
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard(){
+    this.pickCardAnimation = true;
+  }
+}
